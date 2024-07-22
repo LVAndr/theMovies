b1a5ca05b4c4530211ff837d10a33c7cad499ad2
vadim <vadim.lylko@gmail.com>
2024-07-22 23:53:30 +0300

    refactoring - changing style to Composition API

--- src/stores/SearchStore.js
+++ src/stores/SearchStore.js
@@ -1,25 +1,29 @@
 import {defineStore} from "pinia";
 import {useMovieStore} from "./MovieStore.js";
+import {ref} from "vue";
 
 const url =
     'https:api.themoviedb.org/3/search/movie?api_key=02708c4929ad93aa5e68f8ee7bfa4445&query=';
 
-export const useSearchStore = defineStore('searchStore', {
-    state: ()=>({
-        loader: false,
-        movies: []
-    }),
-    actions:{
-        async getMovies(search){
-            this.loader = true;
+export const useSearchStore =
+    defineStore('searchStore', ()=>{
+        const loader = ref(false);
+        const movies = ref([]);
+
+        const getMovies = async (search)=>{
+            loader.value = true;
             const res = await fetch(`${url}${search}`);
             const data = await res.json();
-            this.movies = data.results;
-            this.loader = false;
-        },
-        addToUserMovies(object){
+            movies.value = data.results;
+            loader.value = false;
+        }
+
+        const addToUserMovies = (object)=>{
             const moviesStore = useMovieStore();
             moviesStore.movies.push({...object, isWatched: false});
         }
-    }
-});
+
+        return{
+            loader, movies, getMovies, addToUserMovies
+        }
+})
