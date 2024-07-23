bf1576262964fa078d0516325c30196512211de3
vadim <vadim.lylko@gmail.com>
2024-07-23 13:48:53 +0300

    add localStorage

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -1,10 +1,15 @@
 import {defineStore} from "pinia";
-import {ref, computed} from "vue";
+import {ref, computed, watch} from "vue";
 
 export const useMovieStore =
     defineStore('movieStore', ()=>{
         const movies = ref([]);
         const activeTab = ref(2);
+
+        const moviesInLocalStorage = localStorage.getItem('movies');
+        if (moviesInLocalStorage) {
+            movies.value = JSON.parse(moviesInLocalStorage)._value;
+        }
 
         const watchedMovies = computed(()=>{
             return movies.value.filter((el) => el.isWatched)
@@ -23,6 +28,11 @@ export const useMovieStore =
         const deleteMovie = (id) =>{
             movies.value = movies.value.filter(el => el.id !== id);
         }
+
+        watch(()=> movies,
+            (state)=>{
+            localStorage.setItem('movies', JSON.stringify(state));
+        }, {deep: true})
 
         return{
             movies, activeTab, watchedMovies, totalCountMovies, setActiveTab, toggleWatched, deleteMovie
